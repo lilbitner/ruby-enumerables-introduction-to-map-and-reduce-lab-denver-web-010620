8f05cb1dbda06a1cbc53d1b112276a4397452165
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-06 21:47:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,47 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+source_array*-1 
+  puts source_array
+end 
+
+def map_to_no_change(source_array)
+  puts source_array
+end 
+
+def map_to_double(source_array)
+  source_array*2 
+  puts source_array 
+end 
+
+map_to_square(source_array)
+  source_array*source_array 
+  puts source_array
+end 
+
+
+def reduce_to_total(source_array,starting_point)
+  total = starting_point + source_array 
+  puts total 
+end 
+
+def reduce_to_all_true(source_array)
+  if source_array [index] = true   
+  puts truthy 
+  if source_array[index] = false 
+  puts falsy 
+end 
+
+def reduce_to_any_true(source_array)
+  if source_array[index] = true 
+    puts true 
+  else  
+    puts false 
+end 
+
+
+   
+
+ 
+
+
